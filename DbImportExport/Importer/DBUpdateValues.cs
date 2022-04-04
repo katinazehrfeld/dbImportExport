5cc4f97815dd94370f6bfc90631f671dfb0cfd9e
katina <katinazehrfeld@gmx.de>
2022-04-04 12:48:39 +0200

    Neue Spalten (+/-0,1 Minuten-Werte) in der PaekTabelle

--- DbImportExport/Importer/DBUpdateValues.cs
+++ DbImportExport/Importer/DBUpdateValues.cs
@@ -17,25 +17,22 @@ namespace DbImportExport.Importer        //Namensklasse in der keine Namensgleic
         {
             Log = log;              // zeigt die erfolgten ToDos und Fehler
 
-
-
-
-            ////RT(korrigiert) berechnen und als neue Spalte in SQL einfügen
-            var connection = new SqlConnection("Data Source = KATINALAPTOP2; Initial Catalog = BWB; Integrated Security = true; ");  //verbindet zur entsprechenden DB
+            var connection = new SqlConnection("Data Source = KATINALAPTOP2; Initial Catalog = BWB; Integrated Security = true; ");  //Variable definiert für Verbindung zu entsprechender DB
             connection.Open();               //öffnet gewählte DB
 
             log("Updating data");
 
-
-
             RtKorrigieren(connection);
             RiKorrigieren(connection);
             MzRtName(connection);
+            MzRtp01Name(connection);
+            MzRtm01Name(connection);
+            //MzRtp02Name(connection);
+            //MzRtm02Name(connection);
             MzRiName(connection);
-            //Peak_BWok(connection);
+            //PeaksMinusBW(connection);
+           
         }
-
-
 
 
 
@@ -147,7 +144,7 @@ namespace DbImportExport.Importer        //Namensklasse in der keine Namensgleic
                             SELECT
 	                            messung.ID_Peak,
 	                            messung.RImess,
-	                            probeInfo.RI_IS_Pr 
+	                            probeInfo.RI_IS_Pr      -- alle PEAK-IDs, alle RI-Messungen, alle RI-IS-Messungen aller Proben, als 3Spaltentabelle, verknüpft über PKenng, wenn RIkorr leer und RI_IS nicht leer
                             FROM
 	                            dbo.tbPeaks messung
 	                            LEFT JOIN dbo.tbPInfos probeInfo ON messung.PKenng = probeInfo.PKenng
@@ -157,12 +154,12 @@ namespace DbImportExport.Importer        //Namensklasse in der keine Namensgleic
                             ";
 
 
-            var ids = new List<int>();
-            var riNeu = new List<double>();
+            var ids = new List<int>();                      //int-Liste wird erstellt
+            var riNeu = new List<double>();                 //Double-Liste wird erstellt
 
-            int c = 0;
+            int c = 0;              //int-Counter erstellt mit Null-WErt
 
-            using (var command = connection.CreateCommand())
+            using (var command = connection.CreateCommand())    
             {
                 command.CommandText = sql_select;
                 using (var reader = command.ExecuteReader())
@@ -184,11 +181,11 @@ namespace DbImportExport.Importer        //Namensklasse in der keine Namensgleic
             }
 
             c = 0;
-            foreach (var idMessung in ids)
+            foreach (var idMessung in ids)      // Schleife, um jede ID Zeile zu durchlaufen
             {
                 var riKorr = riNeu[c];
 
-                UpdateRiLine(connection, idMessung, riKorr);
+                UpdateRiLine(connection, idMessung, riKorr);  //hier Übergabe der Werte an SQL
 
                 c++;
             }
@@ -333,6 +330,207 @@ namespace DbImportExport.Importer        //Namensklasse in der keine Namensgleic
         }
 
 
+        // Spalte mit den BPMZ-Rt_plus 0,1min-Namen für jeden Peak, aber nicht BWs
+        private void MzRtp01Name(SqlConnection connection)
+        {
+            var sql_select = @"
+                            SELECT
+	                            messung.ID_Peak,
+	                            messung.RTkorr,
+	                            messung.BP_MZ
+                            FROM
+	                            dbo.tbPeaks messung
+	                        WHERE 
+                                messung.BPMZ_RT_p01 IS NULL
+                                AND messung.RTkorr IS NOT NULL
+                                AND messung.Type = 'Sample'
+                            ";
+
+
+            var ids = new List<int>();
+            var mzrtp01Neu = new List<string>();
+
+            int c = 0;
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = sql_select;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        c++;
+                        var id = (int)reader["ID_Peak"];
+                        var korrRt = (double)reader["RTkorr"];
+                        var Mz = (double)reader["BP_MZ"];
+
+
+
+
+                        //hier rechnen
+                        var mzrtp01NeuValue = SetztMzRtp01(korrRt, Mz);    //Sprung in die Berechnung,siehe unten (mit erforderlichen Parametern)
+
+                        ids.Add(id);
+                        mzrtp01Neu.Add(mzrtp01NeuValue);
+                    }
+                }
+            }
+
+            c = 0;
+            foreach (var idMessung in ids)
+            {
+                string mzrtp01Wert = mzrtp01Neu[c];
+
+                UpdateMzRtp01Line(connection, idMessung, mzrtp01Wert); //
+
+                c++;
+            }
+
+        }
+
+
+
+
+        private string SetztMzRtp01(double korrRt, double Mz)    //rausgezogene Berechnung
+        {
+            Mz = Math.Round(Mz, 0);
+            korrRt = korrRt + 0.1;      //hier Korrektur + 0,1 Minute
+
+            string mzrtp01NeuValue = (Mz + "-" + korrRt);
+
+
+
+            return mzrtp01NeuValue;
+        }
+
+
+
+
+        private void UpdateMzRtp01Line(SqlConnection connection, int idMessung, string mzrtp01Wert)
+        {
+            var sqlUpdateRow = @"UPDATE dbo.tbPeaks
+                        SET 
+                            BPMZ_RT_p01 = @BPMZ_RT_p01
+                        WHERE 
+                            ID_Peak = @ID_Peak
+            ";
+
+
+            using (var commandUpdate = connection.CreateCommand())
+            {
+                commandUpdate.CommandText = sqlUpdateRow;
+
+                commandUpdate.Parameters.AddWithValue("@ID_Peak", idMessung);
+                commandUpdate.Parameters.AddWithValue("@BPMZ_RT_p01", mzrtp01Wert);
+
+                var result = commandUpdate.ExecuteNonQuery();
+
+                Log($"Updated {result} lines");
+            }
+        }
+
+
+
+        // Spalte mit den BPMZ-Rt_minus 0,1min-Namen für jeden Peak, aber nicht BWs
+        private void MzRtm01Name(SqlConnection connection)
+        {
+            var sql_select = @"
+                            SELECT
+	                            messung.ID_Peak,
+	                            messung.RTkorr,
+	                            messung.BP_MZ
+                            FROM
+	                            dbo.tbPeaks messung
+	                        WHERE 
+                                messung.BPMZ_RT_m01 IS NULL
+                                AND messung.RTkorr IS NOT NULL
+                                AND messung.Type = 'Sample'
+                            ";
+
+
+            var ids = new List<int>();
+            var mzrtm01Neu = new List<string>();
+
+            int c = 0;
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = sql_select;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        c++;
+                        var id = (int)reader["ID_Peak"];
+                        var korrRt = (double)reader["RTkorr"];
+                        var Mz = (double)reader["BP_MZ"];
+
+
+
+
+                        //hier rechnen
+                        var mzrtm01NeuValue = SetztMzRtm01(korrRt, Mz);    //Sprung in die Berechnung,siehe unten (mit erforderlichen Parametern)
+
+                        ids.Add(id);
+                        mzrtm01Neu.Add(mzrtm01NeuValue);
+                    }
+                }
+            }
+
+            c = 0;
+            foreach (var idMessung in ids)
+            {
+                string mzrtm01Wert = mzrtm01Neu[c];
+
+                UpdateMzRtm01Line(connection, idMessung, mzrtm01Wert); //
+
+                c++;
+            }
+
+        }
+
+
+
+
+        private string SetztMzRtm01(double korrRt, double Mz)    //rausgezogene Berechnung
+        {
+            Mz = Math.Round(Mz, 0);
+            korrRt = korrRt - 0.1;      //hier Korrektur - 0,1 Minute
+
+            string mzrtm01NeuValue = (Mz + "-" + korrRt);
+
+
+
+            return mzrtm01NeuValue;
+        }
+
+
+
+
+        private void UpdateMzRtm01Line(SqlConnection connection, int idMessung, string mzrtm01Wert)
+        {
+            var sqlUpdateRow = @"UPDATE dbo.tbPeaks
+                        SET 
+                            BPMZ_RT_m01 = @BPMZ_RT_m01
+                        WHERE 
+                            ID_Peak = @ID_Peak
+            ";
+
+
+            using (var commandUpdate = connection.CreateCommand())
+            {
+                commandUpdate.CommandText = sqlUpdateRow;
+
+                commandUpdate.Parameters.AddWithValue("@ID_Peak", idMessung);
+                commandUpdate.Parameters.AddWithValue("@BPMZ_RT_m01", mzrtm01Wert);
+
+                var result = commandUpdate.ExecuteNonQuery();
+
+                Log($"Updated {result} lines");
+            }
+        }
+
+
 
 
         // Spalte mit den BPMZ-RI-Namen für jeden Peak
@@ -439,4 +637,123 @@ namespace DbImportExport.Importer        //Namensklasse in der keine Namensgleic
 
 
 }
+
+    // BW Peak von ProbenPeak abziehen, im Bereich von +/- 0,2min, wenn Werte vorhanden, sonst Null, Kontrolle ob 2x größer, Zelle leer, wenn Werte fehlen
+    /*
+     
+    private void PeaksMinusBW(SqlConnection connection)
+    {
+        int cc = 0;
+
+        var sql_select = @"
+                        SELECT
+                            messung.PKenng,
+                            messung.RTkorr,
+                            messung.BP_MZ,
+                            messung.BPMZ_RT,
+                            messung.AreaP,
+                            messung.Type,
+                            messung.ID_Peak
+                        FROM
+                            dbo.tbPeaks messung
+                            LEFT JOIN dbo.tbPInfos probeInfo ON messung.PKenng = probeInfo.PKenng
+
+                        WHERE 
+                            messung.Peak_minus_BW  IS NULL  --die Zahl 0 bedeutet BWPeak, leere Zelle kann zwei Bedeutungen haben: 1.werte zur Berechung fehlen, 2.neuerEintrag der noch in die Berechnung muss
+                                                            --für fehlende Werte ein FW eintragen, dann kann später, danach selektiert werden, ist das gut?
+                            AND  messung.RTkorr    IS NOT NULL
+                            AND  messung.BPMZ_RT   IS NOT NULL
+
+                        ";
+
+
+        var ids = new List<int>();
+        var PeakminusBWNeu = new List<string>();
+
+        int c = 0;
+
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = sql_select;
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    c++;
+                    var id = (string)reader["Pkenng"];
+                    var korrRt = (double)reader["RTkorr"];
+                    var Mz = (double)reader["BP_MZ"];
+                    var Mz_Rt = (string)reader["BPMZ_RT"];
+                    var PK_Area = (double)reader["AreaP"];
+
+
+
+                    var PeakminusBWNeuValue = Sub_PK_BW(korrRt, Mz, Mz_Rt,PK_Area);    //Sprung in die Berechnung,siehe unten (mit erforderlichen Parametern)
+
+                    ids.Add(id);
+                    PeakminusBWNeu.Add(PeakminusBWNeuValue);
+                }
+            }
+        }
+
+        c = 0;
+        foreach (var PKenng in ids)
+        {
+            double PeakminusBW_Wert = PeakminusBWNeu[c];
+
+            UpdatePeakminusBWLine(connection, PKenng, PeakminusBW_Wert);
+
+            c++;
+        }
+
+    }
+
+
+
+
+    private string Sub_PK_BW(double korrRt, double Mz, string Mz_Rt, double PK_Area)    //rausgezogene Berechnung
+    {
+        Mz = Math.Round(Mz, 0);
+
+        double PeakminusBWNeuValue = (Mz + "-" + korrRt);
+
+
+
+        return PeakminusBWNeuValue;
+    }
+
+
+
+
+    private void UpdatePeakminusBWLine(SqlConnection connection, int idMessung, double PeakminusBW_Wert)
+    {
+        var sqlUpdateRow = @"UPDATE dbo.tbPeaks
+                    SET 
+                        Peak_minus_BW = @Peak_minus_BW
+                    WHERE 
+                        PKenng = @PKenng
+        ";
+
+
+        using (var commandUpdate = connection.CreateCommand())
+        {
+            commandUpdate.CommandText = sqlUpdateRow;
+
+            commandUpdate.Parameters.AddWithValue("@ID_Messung", idMessung);
+            commandUpdate.Parameters.AddWithValue("@Peak_minus_BW", PeakminusBW_Wert);
+
+            var result = commandUpdate.ExecuteNonQuery();
+
+            Log($"Updated {result} lines");
+        }
+    }
+
+
+
+
+
+    
+    */
+
+
 }
